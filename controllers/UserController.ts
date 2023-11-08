cd6ed479ccd52b61dc443d3b9114eb1f330f7e78
Matheus Couto <git.matheusvcouto@gmail.com>
2023-11-08 15:01:02 -0300

    add persist in database

--- controllers/UserController.ts
+++ controllers/UserController.ts
@@ -1,17 +1,18 @@
-import { database } from "../database/db"
+import { Database } from "../database/db"
 import  AppError  from "../utils/serverError"
 import { FastifyReply, FastifyRequest } from "fastify"
 import { User } from "../types/users"
+import { randomUUID } from "crypto"
 
 
-const db = new database()
+const db = new Database()
 
 
 // procurar tipagem para estender a classe com os metodos do fastify
 
 export class UserController {
 
-  list(req: FastifyRequest, reply:FastifyReply) {
+  list() {
     const users = db.select('users')
     return users
   }
@@ -28,7 +29,8 @@ export class UserController {
       throw new AppError('password é obrigatório', 401)
     }
 
-    const user: User = { 
+    const user: User = {
+      id: randomUUID(),
       name, 
       email, 
       password, 
--- database/db.ts
+++ database/db.ts
@@ -1,13 +1,32 @@
-import { User } from "../types/users"
+import path from 'node:path';
+import { User } from '../types/users'
+import fs from 'node:fs/promises'
+
 
 type Record = {
   [key: string]: any;
 };
 
+const dataBasePath = path.resolve('./', './database/database.json') // caminho do meu database
 
-export class database {
+// console.log(dataBasePath) 
+
+
+export class Database {
   private database: { [key: string]: User[] | Record[] } = {
 
+  }
+
+  constructor() { // Sempre que iniciar o servidor ele vai ler o arquivo
+    fs.readFile(dataBasePath, 'utf8').then(data => { // quando terminar de ler o arquivo vai salvar no meu database.json
+      this.database = JSON.parse(data)
+    }).catch(() => { // caso não exista vai criar o arquivo mesmo assim
+      this.persist() 
+    })
+  }
+
+  private persist() {
+    fs.writeFile(dataBasePath, JSON.stringify(this.database, null, 2))
   }
 
   select(table:string) {
@@ -16,6 +35,22 @@ export class database {
     return data
   }
 
+  showDatabase() {
+    return this.database
+  }
+  /*
+  exemple 
+    {
+    "users": [
+      {
+        "name": "dasdasdsd",
+        "email": "sddasasdasa",
+        "password": "madsdasdasddsada"
+      }
+    ]
+  }
+  */
+
   insert(table:string, data: User) {
     if (Array.isArray(this.database[table])) { // se ja existe a tabela 
       this.database[table].push(data)
@@ -23,6 +58,8 @@ export class database {
     } else {
       this.database[table] = [data] 
     }
+
+    this.persist()
 
     return data
   }
